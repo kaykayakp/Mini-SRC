4a25262196fca864a83591e6bd22c6c4fcfd77b6
kaykayakp <22lbwc@queensu.ca>
2026-02-03 14:11:31 -0500

    register.v fix

--- PHASE 1/register.v
+++ PHASE 1/register.v
@@ -1,7 +1,7 @@
-module register #(parameter DATA_WIDTH_IN = 8, DATA_WIDTH_OUT = 8, INIT = 8'h0)(
+module register #(parameter DATA_WIDTH_IN = 32, DATA_WIDTH_OUT = 32, INIT = 32'h0)(
 	input clear, clock, enable, 
 	input [DATA_WIDTH_IN-1:0]BusMuxOut,
-	output wire [DATA_WIDTH_OUT-1:0]BusMuxIn
+	output wire [DATA_WIDTH_OUT-1:0]BusMuxIn_R1
 );
 reg [DATA_WIDTH_IN-1:0]q;
 initial q = INIT;
@@ -14,5 +14,5 @@ always @ (posedge clock)
 				q <= BusMuxOut;
 			end
 		end
-	assign BusMuxIn = q[DATA_WIDTH_OUT-1:0];
+	assign BusMuxIn_R1 = q[DATA_WIDTH_OUT-1:0];
 endmodule
