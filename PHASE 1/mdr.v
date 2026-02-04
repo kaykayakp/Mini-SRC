5e5c1ff4efd55d8c99b3ae08d6a8565331312334
kaykayakp <22lbwc@queensu.ca>
2026-02-04 12:27:59 -0500

    mdr.v

---
+++ PHASE 1/mdr.v
@@ -0,0 +1,26 @@
+module mdr(
+    input wire clock, clear,MDRin,Read,            
+    input wire [31:0] BusMuxOut,
+    input wire [31:0] Mdatain,  
+    output [31:0] BusMuxIn_MDR        
+
+    reg [31:0]q;
+
+    always @(posedge clock) begin
+        if (clear) begin
+            q <= 32'b0;
+        end
+        else if(MDRin) begin 
+            if (Read)
+                q <= Mdatain;        // load from memory
+            else 
+                q<= BusMuxOut;     // load from bus
+            end
+        end
+    end 
+
+    //connect storage to output port 
+    assign BusMuxIn_MDR = q;
+
+
+endmodule
