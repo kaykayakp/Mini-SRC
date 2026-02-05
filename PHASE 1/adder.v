8f87f313c9362300fe5b0a66cb27a531c973cbad
kaykayakp <22lbwc@queensu.ca>
2026-02-04 23:09:28 -0500

    logical tb

---
+++ PHASE 1/adder.v
@@ -0,0 +1,5 @@
+module ripplecarryadder(
+    input [31:0] A,B,
+    output reg [31:0] CC,
+    input wire Select
+);
--- PHASE 1/datapath.v
+++ PHASE 1/datapath.v
@@ -5,9 +5,10 @@ module datapath(
     HIin, LOin, Zhighin, Zlowin, PCin, MDRin, In_Portin, Coutin, Read, IRin, MARin, Yin, Zin,
     input wire R0out, R1out, R2out, R3out, R4out, R5out, R6out, R7out, R8out, R9out, R10out, R11out, R12out, R13out, R14out, R15out,
     HIout, LOout, Zhighout, Zlowout, PCout, MDRout, In_Portout, Cout,
+    input wire IncPC,
     input wire [31:0]Mdatain,
     input wire[4:0] ALU_Control,
-    output wire [31:0]Out_Portout 
+    output wire [31:0]Out_Portout
 ); 
 
     //internal wires
@@ -15,6 +16,13 @@ module datapath(
     BusMuxIn_R9, BusMuxIn_R10, BusMuxIn_R11, BusMuxIn_R12, BusMuxIn_R13, BusMuxIn_R14, BusMuxIn_R15,
     BusMuxIn_HI, BusMuxIn_LO, BusMuxIn_Zhigh, BusMuxIn_Zlow, BusMuxIn_PC, BusMuxIn_MDR, BusMuxIn_In_Port, BusMuxIn_Cout,
     BusMuxIn_IR, BusMuxIn_MAR, BusMuxIn_Y, ALU_out;
+    wire [31:0] pc_plus1;
+    assign pc_plus1 = BusMuxIn_PC + 32'd1;   
+
+    // choose what goes into Z
+    wire [31:0] z_in;
+    assign z_in = (IncPC) ? pc_plus1 : ALU_out;
+
     wire[63:0]ALU_out_wide;
 
     //general purpose registers
@@ -40,7 +48,7 @@ module datapath(
     register ir(clear, clock, IRin, BusMuxOut, BusMuxIn_IR);
     register mar(clear, clock, MARin, BusMuxOut, BusMuxIn_MAR);
     register y(clear, clock, Yin, BusMuxOut, BusMuxIn_Y);
-    register z_low(clear, clock, Zin, ALU_out, BusMuxIn_Zlow);
+    register z_low(clear, clock, Zin, z_in, BusMuxIn_Zlow);
 
     //connect MDR unit 
     mdr mdr_unit(clock, clear, MDRin, Read, BusMuxOut, Mdatain, BusMuxIn_MDR);
